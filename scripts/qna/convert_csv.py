2f7883ae7146f8288546553540c288f6e5514585
Henrique Magarotto <magarotto@gmail.com>
2026-04-29 21:43:06 -0300

    Adjust qna

--- scripts/qna/convert_csv.py
+++ scripts/qna/convert_csv.py
@@ -6,9 +6,9 @@ i=0
 for row in reader:
     if len(row) >= 4 and row[3] == 'disable': continue;
     i+=1
-    ref=f" <<{row[2]},icon:link[]>>" if len(row)>=3 and row[2] else ""
+    ref=f" <<{row[2]},{{ref-link-text}}>>" if len(row)>=3 and row[2] else ""
     print("""\
 [.qna.question]#{count}. {question}#
-ifdef::backend-html5[[.qna.answer_label]#Resposta icon:hand-pointer[]#]
-[.qna.answer_value.hidden]#{answer}{ref}#
-""".format(count=i, question=row[0], answer=row[1], ref=ref))
+ifdef::backend-html5[[.qna.answer_label]#Resposta#]
+[.qna.answer_value.hidden]#*{answer}*{ref}#
+""".format(count=i, question=row[0], answer=row[1].upper(), ref=ref))
